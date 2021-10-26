3fa455dd8b04033e0b0ba506c55a7d9bc648a3a5
Aleksey Loginov <alliaswolf@mail.ru>
2021-10-26 20:15:58 +0300

    Добавление класса CustomUserListView and CustomUserDetailView

--- TODO_List/users_app/views.py
+++ TODO_List/users_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.db.models import query
+from rest_framework.response import Response
+from rest_framework import generics, serializers
+from .models import CustomUser
+from .serializers import CustomUserSerializer
 
 # Create your views here.
+
+
+class CustomUserListView(generics.ListCreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+class CustomUserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
