d192536a76e34bf08ff39262debc0a2f8d66eebb
Aleksey Loginov <alliaswolf@mail.ru>
2021-10-29 20:10:33 +0300

    Создание компонента Footer, применение стилей bootstrap

---
+++ TODO_List/frontend/src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Footer({ textFooter }) {
+  return (
+    <div className="card-footer flex-fill bg-info text-white">{textFooter}</div>
+  );
+}
+export default Footer;
