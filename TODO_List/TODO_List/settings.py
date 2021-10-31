c8ed0595b8b0b285e8177f7278f6d518ea2da0d9
Aleksey Loginov <alliaswolf@mail.ru>
2021-10-31 20:00:33 +0300

    Создание моделей TODO и Project

--- TODO_List/TODO_List/settings.py
+++ TODO_List/TODO_List/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'users_app',
+    'todo_app',
     'corsheaders',
 ]
 
--- TODO_List/todo_app/models.py
+++ TODO_List/todo_app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from users_app.models import CustomUser
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=32, verbose_name="Title of Project")
+    link = models.URLField(null=True,
+                           blank=True,
+                           verbose_name="Link to the repository")
+    users_worked = models.ManyToManyField(CustomUser)
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class TODO(models.Model):
+    title = models.CharField(max_length=32, verbose_name="Title of TODO")
+    text = models.TextField(verbose_name="Text in TODO")
+    is_active = models.BooleanField(default=True,
+                                    verbose_name='Is opened TODO')
+    date_created = models.DateTimeField(auto_now_add=True,
+                                        verbose_name="Date of create TODO")
+    date_modified = models.DateTimeField(auto_now=True,
+                                         verbose_name="Date of modify TODO")
+    author = models.ForeignKey(CustomUser,
+                               on_delete=models.PROTECT,
+                               verbose_name="User created TODO")
+    project = models.ForeignKey(Project,
+                                on_delete=models.PROTECT,
+                                verbose_name="Project")
+
+    class Meta:
+        verbose_name = 'TODO'
+        verbose_name_plural = 'TODOs'
+        ordering = ['title', 'date_created']
+
+    def __str__(self):
+        return self.title
