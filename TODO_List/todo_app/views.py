488e206b6879568232a3c1f276e1064a890b6e77
Aleksey Loginov <alliaswolf@mail.ru>
2021-11-01 22:44:33 +0300

    Создание классов ProjectViewSet и TODOViewSet, подключение RENDERER_CLASSES

--- TODO_List/todo_app/views.py
+++ TODO_List/todo_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import renderer_classes
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from .models import Project, TODO
+from .serializers import ProjectSerializer, TODOSerializer
 
 # Create your views here.
+
+
+class ProjectViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class TODOViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    queryset = TODO.objects.all()
+    serializer_class = TODOSerializer
