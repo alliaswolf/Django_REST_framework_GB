e1ca0b11e5a2f96808e3356f95f967d9d95f3ff5
Aleksey Loginov <alliaswolf@mail.ru>
2021-11-01 22:37:08 +0300

    Добавление url в проекте и для приложения todo_app

---
+++ TODO_List/todo_app/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+from .views import ProjectViewSet, TODOViewSet
+
+router = DefaultRouter()
+router.register('project', ProjectViewSet)
+router.register('todo', TODOViewSet)
+
+urlpatterns = router.urls
