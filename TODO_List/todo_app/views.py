34b0f1c93211d525545a0c4733ccf86ec595296b
Aleksey Loginov <alliaswolf@mail.ru>
2021-11-02 22:32:56 +0300

    Подключение и добавление filterset_class и pagination_class для класса ProjectViewSet

--- TODO_List/todo_app/views.py
+++ TODO_List/todo_app/views.py
@@ -1,15 +1,23 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
 from djangorestframework_camel_case.render import CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer
+from .filters import ProjectFilter
 from .models import Project, TODO
 from .serializers import ProjectSerializer, TODOSerializer
 
 # Create your views here.
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
 class ProjectViewSet(ModelViewSet):
     renderer_classes = [CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer]
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class TODOViewSet(ModelViewSet):
