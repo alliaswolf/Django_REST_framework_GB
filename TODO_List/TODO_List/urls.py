97deb9c1bbf8f01f03e242ed2f65317a1cbf7864
Aleksey Loginov <alliaswolf@mail.ru>
2021-11-12 00:21:03 +0300

    Подключение JWT.

--- TODO_List/TODO_List/urls.py
+++ TODO_List/TODO_List/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,9 @@ urlpatterns = [
     path('api/', include('users_app.urls')),
     path('api-token-auth/', views.obtain_auth_token),
     path('api/projects/', include('todo_app.urls')),
+    path('api/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/token/refresh/',
+         TokenRefreshView.as_view(),
+         name='token_refresh'),
 ]
