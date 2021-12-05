09584c0825162e945607dcbb67f0159c8df6410d
Aleksey Loginov <alliaswolf@mail.ru>
2021-12-05 14:11:52 +0300

    Перенос frontend на уровень выше.

--- TODO_List/TODO_List/settings.py
+++ TODO_List/TODO_List/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'TODO_List.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'frontend/build', ],
+        'DIRS': [ BASE_DIR / '../frontend/build', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (BASE_DIR / 'frontend/build/static',)
+STATICFILES_DIRS = (BASE_DIR / '../frontend/build/static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
